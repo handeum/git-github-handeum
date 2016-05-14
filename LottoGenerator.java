0b8da986bfd9d99f94437e90c4c2f80f88dfe828
Kugsang Jeong <kugsang.jeong@gmail.com>
2016-05-14 16:57:00 +0900

    fix bug

--- LottoGenerator.java
+++ LottoGenerator.java
@@ -1,17 +1,21 @@
 pckage project;
+
 import java.util.Random;
 import java.util.Arrays;
+
 public class LottoGenerator {
 private Random random = new Random(System.nanoTime());
+
 public int[] generate() {
-int[] result = new int[6];
-for(int i = 0; i < 6; i++) {
-result[i] = random.nextInt(45);
+  int[] result = new int[6];
+  for(int i = 0; i < 6; i++) {
+    result[i] = random.nextInt(45) + 1;
 }
-return result;
+  return result;
 }
+
 public static void main(String[] args) {
-int[] result = new LottoGenerator().generate();
-System.out.println(Arrays.toString(result));
+   int[] result = new LottoGenerator().generate();
+  System.out.println(Arrays.toString(result));
 }
 }
